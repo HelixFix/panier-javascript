f8a6d38e258d0530a4509bf0423a14774ade4bba
HelixFix <jerome.rinner@gmail.com>
2021-01-01 17:56:16 +0100

    :art: Align code

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function ajout()
         var produit = (document.getElementById("produit").value);
         var qte     = parseInt(document.getElementById("qte").value);
         var prix    = parseFloat(document.getElementById("prix").value);
-        console.log(produit);
-        var total = (prix * qte);
-        var test  = total.toFixed(2)
+        var total   = (prix * qte);
+        var test    = total.toFixed(2)
+
         console.log(total);
+        console.log(produit);
 
         if (produit == "") {
             alert("Selectionne un produit, mon cher !");
@@ -15,19 +16,13 @@ function ajout()
 
         if(prix > 0 ) 
         { 
-        alert('Correct..')
-        
+        console.log('Prix correct');
         }
         else
         { 
-        alert('Pas bon...!')
+        alert('Un prix négatif ? Try Again')
         return false;
         }
-
-
-    
-        
-        
 
         document.getElementById("tableau").innerHTML += "<tr><td>" + produit + "</td><td>" + qte + "</td><td>" + test + "</td></tr>";
         document.getElementById("tableau").innerHTML += "<tr><td> </td></tr>";
