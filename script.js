a24b3706754313be2b8e3c71777709c28533a0bc
HelixFix <jerome.rinner@gmail.com>
2021-01-01 17:50:19 +0100

    :construction: In progress

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 function ajout()
-            {
-                var produit = (document.getElementById("produit").value);
-                var qte     = parseInt(document.getElementById("qte").value);
-                var prix     = parseInt(document.getElementById("prix").value);
-                console.log(produit);
-                var total = (prix * qte);
-                console.log(total);
+    {
+        var produit = (document.getElementById("produit").value);
+        var qte     = parseInt(document.getElementById("qte").value);
+        var prix    = parseFloat(document.getElementById("prix").value);
+        console.log(produit);
+        var total = (prix * qte);
+        var test  = total.toFixed(2)
+        console.log(total);
 
-                if (produit == "") {
-                  alert("Selectionne un produit, mon cher !");
-                  return false;
-                }
+        if (produit == "") {
+            alert("Selectionne un produit, mon cher !");
+            return false;
+        }
 
-           
-                
-               
+        if(prix > 0 ) 
+        { 
+        alert('Correct..')
+        
+        }
+        else
+        { 
+        alert('Pas bon...!')
+        return false;
+        }
 
-                document.getElementById("tableau").innerHTML += "<tr><td>" + produit + "</td><td>" + qte + "</td><td>" + total + "</td></tr>";
-                document.getElementById("tableau").innerHTML += "<tr><td> </td></tr>";
-                // document.getElementById("prixTotal").innerHTML += 
-                
 
-            } 
+    
+        
+        
+
+        document.getElementById("tableau").innerHTML += "<tr><td>" + produit + "</td><td>" + qte + "</td><td>" + test + "</td></tr>";
+        document.getElementById("tableau").innerHTML += "<tr><td> </td></tr>";
+        // document.getElementById("prixTotal").innerHTML += 
+        
+
+    } 
