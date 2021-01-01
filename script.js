d353aaa6fd86a0e7578f50d2260cd8dbb07e4404
HelixFix <jerome.rinner@gmail.com>
2021-01-01 16:44:14 +0100

    :construction: Add product verification

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-
-
 function ajout()
             {
                 var produit = (document.getElementById("produit").value);
                 var qte     = parseInt(document.getElementById("qte").value);
+                var prix     = parseInt(document.getElementById("prix").value);
                 console.log(produit);
+                var total = (prix * qte);
+                console.log(total);
+
+                if (produit == "") {
+                  alert("Selectionne un produit, mon cher !");
+                  return false;
+                }
+
+           
+                
                
 
-                document.getElementById("tableau").innerHTML += "<tr><td>" + produit + "</td><td>" + qte + "</td><td>" + qte + "</td></tr>";
-           
+                document.getElementById("tableau").innerHTML += "<tr><td>" + produit + "</td><td>" + qte + "</td><td>" + total + "</td></tr>";
+                document.getElementById("tableau").innerHTML += "<tr><td> </td></tr>";
+                // document.getElementById("prixTotal").innerHTML += 
+                
 
- 
-            }
+            } 
